1998d273c92c3a9f544adb21e4ee48ba2b8bef61
LUI-TECH <bojia.mao16@imperial.ac.uk>
2021-01-10 22:26:38 +0000

    1

---
+++ given/stamp.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <openssl/sha.h>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <fstream>
+#include "stamp.h"
+
+using namespace std;
+
+/* You are pre-supplied with the functions below. Add your own 
+   function definitions to the end of this file. */
+
+// helper function for internal use only
+// transforms raw binary hash value into human-friendly hexademical form
+void convert_hash(const unsigned char *str, char *output, int hash_length) {
+  char append[16];
+  strcpy (output, "");
+  for (int n=0; n<hash_length; n++) {
+    sprintf(append,"%02x",str[n]);
+    strcat(output, append);
+  }
+}
+
+// pre-supplied helper function
+// generates the SHA1 hash of input string text into output parameter digest
+// ********************** IMPORTANT **************************
+// ---> remember to include -lcrypto in your linking step <---
+// ---> so that the definition of the function SHA1 is    <---
+// ---> included in your program                          <---
+// ***********************************************************
+void text_to_SHA1_digest(const char *text, char *digest) {
+  unsigned char hash[SHA_DIGEST_LENGTH];
+  SHA1( (const unsigned char *) text, strlen(text), hash);
+  convert_hash(hash, digest, SHA_DIGEST_LENGTH);
+}
+
+/* add your function definitions here */
+
+int leading_zeros(string digest){
+  int count =0;
+  for (int i = 0; i < 5; i++ ){
+    if ((digest[i] < '0' || digest[i] > '9') && (digest[i] < 'a' || digest[i] > 'f')){
+      return -1 ;
+    }
+    if (digest[i]==0){
+      count++;
+    }
+  }
+  return count;
+}
+
+
+bool file_to_SHA1_digest(string filename, char * digest){
+  ifstream file(filename);
+  if (!file.is_open()){
+    digest[0] = 'e';
+    digest[1] = 'r';
+    digest[2] = 'r';
+    digest[3] = 'o';
+    digest[4] = 'r';
+    return false;
+  }
+  char text[100];
+  file>>text;
+  text_to_SHA1_digest(text,digest);
+  return true;
+}
---
+++ given/stamp.h
@@ -0,0 +1,13 @@
+enum MessageStatus {
+  INVALID_HEADER=-1,
+  WRONG_RECIPIENT=-2,
+  INVALID_MESSAGE_DIGEST=-3,
+  INVALID_HEADER_DIGEST=-4,
+  VALID_EMAIL=1
+};
+
+/* pre-supplied helper function */
+/* generates the SHA1 hash of input string text into output parameter digest */
+void text_to_SHA1_digest(const char *text, char *digest);
+int leading_zeros(std::string digest);
+bool file_to_SHA1_digest(std::string filename, char * digest);
